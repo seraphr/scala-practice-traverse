5c2e1e1f9d7cb2ff9910aab8c0768165b8fcd03f
seraph <local@seraphr.jp>
2013-04-07 00:24:01 +0900

    テスト内容を少し追加

--- src/test/scala/jp/seraphr/traverse/data/StateTest.scala
+++ src/test/scala/jp/seraphr/traverse/data/StateTest.scala
@@ -5,8 +5,17 @@ import org.scalatest.FunSuite
 class StateTest extends FunSuite {
   test("count with State") {
     val count = (i: Int) => State((s: Int) => (s + 1, i))
-    val tResult = count(10).flatMap(count).flatMap(count).runState(0)
+    val tAdd3 = count(10).flatMap(count).flatMap(count);
+    val tResult = tAdd3.runState(0)
 
     assert(tResult === (3, 10))
+
+    val tAdd5 = for {
+      i <- tAdd3
+      _ <- count(i)
+      _ <- count(i)
+    } yield 3
+
+    assert(tAdd5.runState(0) === (5, 3))
   }
 }
